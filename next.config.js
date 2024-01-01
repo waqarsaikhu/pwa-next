360ce8c7faf3d863127244e18199dbc9b52a767a
waqarsaikhu <wsaikhou16@gmail.com>
2024-01-01 11:50:07 +0500

    PWANEXT

--- next.config.js
+++ next.config.js
@@ -6,6 +6,4 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+module.exports = withPWA({});
